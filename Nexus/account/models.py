e41fdf0a6ff492f5f1f797f17547f1dd459d7665
mitaliborad <mitaliia109@gmail.com>
2025-12-24 10:31:09 +0530

    add Login API with jwt authentication

--- Nexus/account/models.py
+++ Nexus/account/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .managers import CustomUserManager
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class User(AbstractUser):
     username = None
@@ -13,6 +14,14 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.email
+    
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        print(refresh)
+        return{
+            'refresh' : str(refresh),
+            'access' : str(refresh.access_token)
+        }
     
 
 
--- Nexus/account/views.py
+++ Nexus/account/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.renderers import JSONRenderer
-
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
 from .models import User
-from .serializers import UserSerializer
+from .serializers import UserSerializer, LoginSerializer
 
 class UserCreateView(generics.CreateAPIView):
+    permission_classes = [AllowAny]
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+
 
 
 
