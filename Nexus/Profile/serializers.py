4ce9286c04e124841ba20e0d8ace74ce194e8c50
mitaliborad <mitaliia109@gmail.com>
2025-12-25 17:53:40 +0530

    add username in user profile API

--- Nexus/Profile/serializers.py
+++ Nexus/Profile/serializers.py
@@ -2,12 +2,6 @@ from rest_framework import serializers
 from account.models import User
 
 class UserProfileSerializer(serializers.ModelSerializer):
-    name = serializers.SerializerMethodField()
-
-    def get_name(self, obj):
-        return obj.email.split('@')[0]
-
     class Meta:
         model = User
-        fields = ['name', 'bio', 'profile_picture']
-        read_only_fields = ['name'] 
+        fields = ['username', 'bio', 'profile_picture']
